42a0b9f104514e8d9c1e79ff662a0a9586a6f3e0
Josh Gwosdz <jgwosdz@redhat.com>
2021-05-04 17:07:03 +0200

    default addon phase to "Pending"
    Signed-off-by: Josh Gwosdz <jgwosdz@redhat.com>

--- apis/addons/v1alpha1/addons_types.go
+++ apis/addons/v1alpha1/addons_types.go
@@ -48,7 +48,8 @@ type Addon struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   AddonSpec   `json:"spec,omitempty"`
+	Spec AddonSpec `json:"spec,omitempty"`
+	// +kubebuilder:default={phase:Pending}
 	Status AddonStatus `json:"status,omitempty"`
 }
 
