9b872a2518114d266deeba29e4c62ba11f0fc8d1
Josh Gwosdz <jgwosdz@redhat.com>
2021-12-07 12:24:06 +0100

    assert that all addons are gone when test teardown starts
    Signed-off-by: Josh Gwosdz <jgwosdz@redhat.com>

--- integration/teardown_test.go
+++ integration/teardown_test.go
@@ -4,11 +4,23 @@ import (
 	"context"
 	"time"
 
+	addonsv1alpha1 "github.com/openshift/addon-operator/apis/addons/v1alpha1"
 	"github.com/openshift/addon-operator/integration"
 )
 
 func (s *integrationTestSuite) Teardown() {
 	ctx := context.Background()
+
+	// assert that all addons are gone before teardown
+	addonList := &addonsv1alpha1.AddonList{}
+	err := integration.Client.List(ctx, addonList)
+	s.Assert().NoError(err)
+	addonNames := []string{}
+	for _, a := range addonList.Items {
+		addonNames = append(addonNames, a.GetName())
+	}
+	s.Assert().Len(addonNames, 0, "expected all Addons to be gone before teardown, but some still exist")
+
 	objs := integration.LoadObjectsFromDeploymentFiles(s.T())
 
 	// reverse object order for de-install
@@ -20,7 +32,7 @@ func (s *integrationTestSuite) Teardown() {
 	for _, obj := range objs {
 		o := obj
 		err := integration.Client.Delete(ctx, &o)
-		s.Require().NoError(err)
+		s.Assert().NoError(err)
 
 		s.T().Log("deleted: ", o.GroupVersionKind().String(),
 			o.GetNamespace()+"/"+o.GetName())
