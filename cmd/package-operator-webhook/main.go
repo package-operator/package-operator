f73912c94f62175e45a0b40f227a7525e7b10ec7
gathomas <gathomas@redhat.com>
2022-08-16 10:29:04 +0200

    add apis to scheme

--- cmd/package-operator-webhook/main.go
+++ cmd/package-operator-webhook/main.go
@@ -8,6 +8,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	corev1alpha1 "package-operator.run/apis/core/v1alpha1"
 )
 
 var (
@@ -15,9 +17,9 @@ var (
 	setupLog = ctrl.Log.WithName("setup")
 )
 
-// func init() {
-//  _ = <>.AddToScheme(scheme) TODO: Add our package with objects to scheme
-// }
+func init() {
+	_ = corev1alpha1.AddToScheme(scheme)
+}
 
 func main() {
 	var (
