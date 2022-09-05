9a928c1cf2077b4a5e74bc0fbe9448848f9bb6d2
Nico Schieder <nschieder@redhat.com>
2022-09-05 11:40:12 +0200

    Remove duplicated tests and use t.Run for sub-tests
    Signed-off-by: Nico Schieder <nschieder@redhat.com>

--- internal/webhooks/generic_objectsetphase_webhook_test.go
+++ internal/webhooks/generic_objectsetphase_webhook_test.go
@@ -8,95 +8,51 @@ import (
 	corev1alpha1 "package-operator.run/apis/core/v1alpha1"
 )
 
-//nolint:dupl
 func TestValidateUpdate_ObjectSetPhase(t *testing.T) {
 	wh := new(GenericObjectSetPhaseWebhookHandler[corev1alpha1.ObjectSetPhase])
 
-	// test Previous immutable
-	oldObj := wh.newObjectSetPhase()
-	obj := wh.newObjectSetPhase()
-	oldObj.Spec.Previous = []corev1alpha1.PreviousRevisionReference{{Name: "previous-revision"}}
-	obj.Spec.Previous = []corev1alpha1.PreviousRevisionReference{{Name: "different-revision"}}
-	r := wh.validateUpdate(obj, oldObj)
-	assert.False(t, r.Allowed)
-	assert.Equal(t, string(r.Result.Reason), errPreviousImmutable.Error())
+	t.Run("previous immutable", func(t *testing.T) {
+		oldObj := wh.newObjectSetPhase()
+		obj := wh.newObjectSetPhase()
+		oldObj.Spec.Previous = []corev1alpha1.PreviousRevisionReference{{Name: "previous-revision"}}
+		obj.Spec.Previous = []corev1alpha1.PreviousRevisionReference{{Name: "different-revision"}}
+		r := wh.validateUpdate(obj, oldObj)
+		assert.False(t, r.Allowed)
+		assert.Equal(t, string(r.Result.Reason), errPreviousImmutable.Error())
+	})
 
-	// test ObjectSetTemplatePhase immutable
-	oldObj = wh.newObjectSetPhase()
-	obj = wh.newObjectSetPhase()
-	oldObj.Spec.ObjectSetTemplatePhase = corev1alpha1.ObjectSetTemplatePhase{Name: "first-phase"}
-	obj.Spec.ObjectSetTemplatePhase = corev1alpha1.ObjectSetTemplatePhase{Name: "second-phase"}
-	r = wh.validateUpdate(obj, oldObj)
-	assert.False(t, r.Allowed)
-	assert.Equal(t, string(r.Result.Reason), errObjectSetTemplatePhaseImmutable.Error())
+	t.Run("ObjectSetTemplatePhase immutable", func(t *testing.T) {
+		oldObj := wh.newObjectSetPhase()
+		obj := wh.newObjectSetPhase()
+		oldObj.Spec.ObjectSetTemplatePhase = corev1alpha1.ObjectSetTemplatePhase{Name: "first-phase"}
+		obj.Spec.ObjectSetTemplatePhase = corev1alpha1.ObjectSetTemplatePhase{Name: "second-phase"}
+		r := wh.validateUpdate(obj, oldObj)
+		assert.False(t, r.Allowed)
+		assert.Equal(t, string(r.Result.Reason), errObjectSetTemplatePhaseImmutable.Error())
+	})
 
-	// test Revision immutable
-	oldObj = wh.newObjectSetPhase()
-	obj = wh.newObjectSetPhase()
-	oldObj.Spec.Revision = 1
-	obj.Spec.Revision = 2
-	r = wh.validateUpdate(obj, oldObj)
-	assert.False(t, r.Allowed)
-	assert.Equal(t, string(r.Result.Reason), errRevisionImmutable.Error())
+	t.Run("revision immutable", func(t *testing.T) {
+		oldObj := wh.newObjectSetPhase()
+		obj := wh.newObjectSetPhase()
+		oldObj.Spec.Revision = 1
+		obj.Spec.Revision = 2
+		r := wh.validateUpdate(obj, oldObj)
+		assert.False(t, r.Allowed)
+		assert.Equal(t, string(r.Result.Reason), errRevisionImmutable.Error())
+	})
 
-	// test AvailabilityProbes immutable
-	oldObj = wh.newObjectSetPhase()
-	obj = wh.newObjectSetPhase()
+	t.Run("AvailabilityProbes immutable", func(t *testing.T) {
+		oldObj := wh.newObjectSetPhase()
+		obj := wh.newObjectSetPhase()
 
-	cp1 := &corev1alpha1.ProbeConditionSpec{Status: "True"}
-	oldObj.Spec.AvailabilityProbes = []corev1alpha1.ObjectSetProbe{{Probes: []corev1alpha1.Probe{{Condition: cp1}}}}
+		cp1 := &corev1alpha1.ProbeConditionSpec{Status: "True"}
+		oldObj.Spec.AvailabilityProbes = []corev1alpha1.ObjectSetProbe{{Probes: []corev1alpha1.Probe{{Condition: cp1}}}}
 
-	cp2 := &corev1alpha1.ProbeConditionSpec{Status: "False"}
-	obj.Spec.AvailabilityProbes = []corev1alpha1.ObjectSetProbe{{Probes: []corev1alpha1.Probe{{Condition: cp2}}}}
+		cp2 := &corev1alpha1.ProbeConditionSpec{Status: "False"}
+		obj.Spec.AvailabilityProbes = []corev1alpha1.ObjectSetProbe{{Probes: []corev1alpha1.Probe{{Condition: cp2}}}}
 
-	r = wh.validateUpdate(obj, oldObj)
-	assert.False(t, r.Allowed)
-	assert.Equal(t, string(r.Result.Reason), errAvailabilityProbesImmutable.Error())
-}
-
-//nolint:dupl
-func TestValidateUpdate_ClusterObjectSetPhase(t *testing.T) {
-	wh := new(GenericObjectSetPhaseWebhookHandler[corev1alpha1.ClusterObjectSetPhase])
-
-	// test Previous immutable
-	oldObj := wh.newObjectSetPhase()
-	obj := wh.newObjectSetPhase()
-	oldObj.Spec.Previous = []corev1alpha1.PreviousRevisionReference{{Name: "previous-revision"}}
-	obj.Spec.Previous = []corev1alpha1.PreviousRevisionReference{{Name: "different-revision"}}
-	r := wh.validateUpdate(obj, oldObj)
-	assert.False(t, r.Allowed)
-	assert.Equal(t, string(r.Result.Reason), errPreviousImmutable.Error())
-
-	// test ObjectSetTemplatePhase immutable
-	oldObj = wh.newObjectSetPhase()
-	obj = wh.newObjectSetPhase()
-	oldObj.Spec.ObjectSetTemplatePhase = corev1alpha1.ObjectSetTemplatePhase{Name: "first-phase"}
-	obj.Spec.ObjectSetTemplatePhase = corev1alpha1.ObjectSetTemplatePhase{Name: "second-phase"}
-	r = wh.validateUpdate(obj, oldObj)
-	assert.False(t, r.Allowed)
-	assert.Equal(t, string(r.Result.Reason), errObjectSetTemplatePhaseImmutable.Error())
-
-	// test Revision immutable
-	oldObj = wh.newObjectSetPhase()
-	obj = wh.newObjectSetPhase()
-	oldObj.Spec.Revision = 1
-	obj.Spec.Revision = 2
-	r = wh.validateUpdate(obj, oldObj)
-	assert.False(t, r.Allowed)
-	assert.Equal(t, string(r.Result.Reason), errRevisionImmutable.Error())
-
-	// test AvailabilityProbes immutable
-	oldObj = wh.newObjectSetPhase()
-	obj = wh.newObjectSetPhase()
-
-	cp1 := &corev1alpha1.ProbeConditionSpec{Status: "True"}
-	oldObj.Spec.AvailabilityProbes = []corev1alpha1.ObjectSetProbe{{Probes: []corev1alpha1.Probe{{Condition: cp1}}}}
-
-	cp2 := &corev1alpha1.ProbeConditionSpec{Status: "False"}
-	obj.Spec.AvailabilityProbes = []corev1alpha1.ObjectSetProbe{{Probes: []corev1alpha1.Probe{{Condition: cp2}}}}
-
-	r = wh.validateUpdate(obj, oldObj)
-	assert.False(t, r.Allowed)
-	assert.Equal(t, string(r.Result.Reason), errAvailabilityProbesImmutable.Error())
-
+		r := wh.validateUpdate(obj, oldObj)
+		assert.False(t, r.Allowed)
+		assert.Equal(t, string(r.Result.Reason), errAvailabilityProbesImmutable.Error())
+	})
 }
