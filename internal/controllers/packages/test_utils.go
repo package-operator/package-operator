9d12adac3f2c1d2732a9dc41658597c2e383fb22
Yashvardhan Kukreja <ykukreja@redhat.com>
2022-10-21 02:35:56 +0530

    chore: unit tests for package controller
    Signed-off-by: Yashvardhan Kukreja <ykukreja@redhat.com>

--- internal/controllers/packages/test_utils.go
+++ internal/controllers/packages/test_utils.go
@@ -1,8 +1,11 @@
 package packages
 
 import (
+	"context"
+
 	"github.com/stretchr/testify/mock"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 )
@@ -41,4 +44,26 @@ func (s *mockOwnerStrategy) IsOwner(owner, obj metav1.Object) bool {
 }
 
 func (s *mockOwnerStrategy) ReleaseController(obj metav1.Object) {
+}
+
+type objectDeploymentStatusReconcilerMock struct {
+	mock.Mock
+}
+
+type jobReconcilerMock struct {
+	mock.Mock
+}
+
+func (r *objectDeploymentStatusReconcilerMock) Reconcile(
+	ctx context.Context, pkg genericPackage,
+) (res ctrl.Result, err error) {
+	args := r.Called(ctx, pkg)
+	return args.Get(0).(ctrl.Result), args.Error(1)
+}
+
+func (r *jobReconcilerMock) Reconcile(
+	ctx context.Context, pkg genericPackage,
+) (res ctrl.Result, err error) {
+	args := r.Called(ctx, pkg)
+	return args.Get(0).(ctrl.Result), args.Error(1)
 }
