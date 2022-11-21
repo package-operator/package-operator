213ba4987a66860a2a7d0bbecb5ac8d6f99928f4
Nico Schieder <nschieder@redhat.com>
2022-11-21 11:35:22 +0100

    Remove sleep :)
    Signed-off-by: Nico Schieder <nschieder@redhat.com>

--- cmd/package-operator-manager/main.go
+++ cmd/package-operator-manager/main.go
@@ -108,8 +108,6 @@ func main() {
 		}
 		if err := runLoader(scheme, packageKey); err != nil {
 			setupLog.Error(err, "unable to run package-loader")
-
-			time.Sleep(1 * time.Hour)
 			os.Exit(1)
 		}
 		return
