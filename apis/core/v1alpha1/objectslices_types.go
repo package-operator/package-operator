12792cbffbd1ccb6a3c2e7b2403e09728105847c
Nico Schieder <nschieder@redhat.com>
2022-11-25 16:17:14 +0100

    Update apis/core/v1alpha1/objectslices_types.go
    Co-authored-by: Garrett Thomas <garrettcarrollthomas@gmail.com>

--- apis/core/v1alpha1/objectslices_types.go
+++ apis/core/v1alpha1/objectslices_types.go
@@ -3,7 +3,7 @@ package v1alpha1
 import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 // ObjectSlices are referenced by ObjectSets or ObjectDeployments and contain objects to
-// limit the size of ObjectSet and ObjectDeployments when big packages are installed.
+// limit the size of ObjectSets and ObjectDeployments when big packages are installed.
 // This is necessary to work around the etcd object size limit of ~1.5MiB and to reduce load on the kube-apiserver.
 // +kubebuilder:object:root=true
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
