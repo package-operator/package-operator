453408069b7798d6f7d43e3d68fbd6955e6d4b95
Nico Schieder <nschieder@redhat.com>
2023-03-31 20:02:44 +0200

    Hotfix: Unstuck PKO self-deploy when hitting status reporting bug
    Signed-off-by: Nico Schieder <nschieder@redhat.com>

--- cmd/package-operator-manager/bootstrap.go
+++ cmd/package-operator-manager/bootstrap.go
@@ -81,6 +81,10 @@ func (b *bootstrapper) Bootstrap(ctx context.Context) error {
 		meta.IsStatusConditionTrue(packageOperatorPackage.Status.Conditions, corev1alpha1.PackageAvailable) {
 		// Package Operator is already installed.
 		b.log.Info("Package Operator already installed, updating via in-cluster Package Operator")
+		if err := b.fixMissingRevisionNumbers(ctx); err != nil {
+			return fmt.Errorf("fix missing revision numbers: %w", err)
+		}
+
 		return b.updatePKOPackage(ctx, packageOperatorPackage)
 	}
 
@@ -91,6 +95,50 @@ func (b *bootstrapper) Bootstrap(ctx context.Context) error {
 
 	b.log.Info("Package Operator NOT Available, self-bootstrapping")
 	return b.selfBootstrap(ctx)
+}
+
+func (b *bootstrapper) fixMissingRevisionNumbers(ctx context.Context) error {
+	clusterObjectSetList := &corev1alpha1.ClusterObjectSetList{}
+	if err := b.client.List(ctx, clusterObjectSetList, client.MatchingLabels{
+		"package-operator.run/instance": "package-operator",
+	}); err != nil {
+		return fmt.Errorf("list PKO ClusterObjectSet: %w", err)
+	}
+
+	cosByName := map[string]*corev1alpha1.ClusterObjectSet{}
+	for i := range clusterObjectSetList.Items {
+		cos := &clusterObjectSetList.Items[i]
+		cosByName[cos.Name] = cos
+	}
+
+	for i := range clusterObjectSetList.Items {
+		cos := &clusterObjectSetList.Items[i]
+		if cos.Status.Revision == 0 &&
+			len(cos.Spec.Previous) > 0 &&
+			cos.Status.Phase == corev1alpha1.ObjectSetStatusPhasePending {
+			// Assume it's stuck
+			cos.Status.Revision = highestRevisionNumber(
+				cosByName, cos.Spec.Previous...) + 1
+			if err := b.client.Status().Patch(ctx, cos, client.Merge); err != nil {
+				return fmt.Errorf("patch for missing revision number: %w", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func highestRevisionNumber(
+	cosByName map[string]*corev1alpha1.ClusterObjectSet,
+	prevs ...corev1alpha1.PreviousRevisionReference,
+) int64 {
+	var maxRevision int64
+	for _, prev := range prevs {
+		if cosByName[prev.Name].Status.Revision > maxRevision {
+			maxRevision = cosByName[prev.Name].Status.Revision
+		}
+	}
+	return maxRevision
 }
 
 func (b *bootstrapper) updatePKOPackage(
