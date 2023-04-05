27d02704952b8437e0005c7ddf214ffa05f8561d
Nico Schieder <nschieder@redhat.com>
2023-04-05 13:28:23 +0200

    Use lazy rest mapper
    In large clusters with many APIs, PKO startup may take a long time
    because we are running into client throttling.
    
    Switch to lazy restmapper to delay apidiscovery until required.
    
    Signed-off-by: Nico Schieder <nschieder@redhat.com>

--- cmd/remote-phase-manager/main.go
+++ cmd/remote-phase-manager/main.go
@@ -157,7 +157,7 @@ func run(log logr.Logger, scheme *runtime.Scheme, opts opts) error {
 	if err != nil {
 		return fmt.Errorf("reading target cluster kubeconfig: %w", err)
 	}
-	targetMapper, err := apiutil.NewDynamicRESTMapper(targetCfg)
+	targetMapper, err := apiutil.NewDynamicRESTMapper(targetCfg, apiutil.WithLazyDiscovery)
 	if err != nil {
 		return fmt.Errorf("creating target cluster rest mapper: %w", err)
 	}
