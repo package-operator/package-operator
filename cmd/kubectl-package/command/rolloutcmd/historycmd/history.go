0a5fb312c7dde6f7bb715294714f90a38a5a0d7b
Adam Ludes <aludes@redhat.com>
2023-04-21 11:59:24 +0200

    getting cluster scoped objects

--- cmd/kubectl-package/command/rolloutcmd/historycmd/history.go
+++ cmd/kubectl-package/command/rolloutcmd/historycmd/history.go
@@ -19,16 +19,17 @@ import (
 )
 
 const (
-	historyUse             = "history object/name"
-	historyShort           = "view object rollout history"
-	historyLong            = "view previous object rollout revisions and configurations"
-	historyClusterScopeUse = "render in cluster scope"
+	historyUse   = "history object/name"
+	historyShort = "view object rollout history"
+	historyLong  = "view previous object rollout revisions and configurations"
+	namespaceUse = "If present, the namespace scope for this CLI request."
 )
 
 type History struct {
-	client client.Client
-	Object string
-	Name   string
+	client    client.Client
+	Object    string
+	Name      string
+	Namespace string
 }
 
 func (h *History) Complete(args []string) error {
@@ -62,9 +63,37 @@ func (h *History) Run(ctx context.Context, out io.Writer) error {
 
 	switch h.Object {
 	case "clusterpackage", "cpkg":
-		fmt.Println("clusterpackage")
+		pkg, err := h.GetClusterPackageByName(ctx, h.Name)
+		if err != nil {
+			return fmt.Errorf("retrieving packages: %w", err)
+		}
+		if pkg == nil {
+			return fmt.Errorf("packages.package-operator.run \"%s\" not found", h.Name)
+		}
+		objdeploy, err := h.GetClusterObjectDeploymentByOwner(ctx, pkg.UID)
+		if err != nil {
+			return fmt.Errorf("retrieving objectdeployments: %w", err)
+		}
+		if objdeploy == nil {
+			return fmt.Errorf("objectdeployment not found with: OwnerReferences.UID=%s", pkg.UID)
+		}
+		objset, err := h.GetClusterObjectSetByOwner(ctx, objdeploy.UID)
+		if err != nil {
+			return fmt.Errorf("retrieving objectsets: %w", err)
+		}
+		if objset == nil {
+			return fmt.Errorf("objectsets not found with: OwnerReferences.UID=%s", objdeploy.UID)
+		}
+		fmt.Println(objset)
 	case "clusterobjectdeployment", "cobjdeploy":
-		fmt.Println("clusterobjectdeployment")
+		objdeploy, err := h.GetClusterObjectDeploymentByName(ctx, h.Name)
+		if err != nil {
+			return fmt.Errorf("creating client: %w", err)
+		}
+		fmt.Println(objdeploy)
+		fmt.Println("")
+		objset, err := h.GetClusterObjectSetByOwner(ctx, objdeploy.UID)
+		fmt.Println(objset)
 	case "package", "pkg":
 		pkg, err := h.GetPackageByName(ctx, h.Name)
 		if err != nil {
@@ -88,7 +117,6 @@ func (h *History) Run(ctx context.Context, out io.Writer) error {
 			return fmt.Errorf("objectsets not found with: OwnerReferences.UID=%s", objdeploy.UID)
 		}
 		fmt.Println(objset)
-
 	case "objectdeployment", "objdeploy":
 		objdeploy, err := h.GetObjectDeploymentByName(ctx, h.Name)
 		if err != nil {
@@ -105,10 +133,75 @@ func (h *History) Run(ctx context.Context, out io.Writer) error {
 	return nil
 }
 
+func (h *History) GetClusterPackageByName(ctx context.Context, name string) (*corev1alpha1.ClusterPackage, error) {
+	var clusterPackageList corev1alpha1.ClusterPackageList
+
+	err := h.client.List(ctx, &clusterPackageList)
+	if err != nil {
+		return nil, fmt.Errorf("getting packages: %w", err)
+	}
+	for _, n := range clusterPackageList.Items {
+		if name == n.Name {
+			return &n, nil
+		}
+	}
+	return nil, nil
+}
+
+func (h *History) GetClusterObjectDeploymentByName(ctx context.Context, name string) (*corev1alpha1.ClusterObjectDeployment, error) {
+	var clusterObjectDeploymentList corev1alpha1.ClusterObjectDeploymentList
+
+	err := h.client.List(ctx, &clusterObjectDeploymentList)
+	if err != nil {
+		return nil, fmt.Errorf("getting objectdeployments: %w", err)
+	}
+	for _, n := range clusterObjectDeploymentList.Items {
+		if name == n.Name {
+			return &n, nil
+		}
+	}
+	return nil, nil
+}
+
+func (h *History) GetClusterObjectDeploymentByOwner(ctx context.Context, ownerUid types.UID) (*corev1alpha1.ClusterObjectDeployment, error) {
+	var clusterObjectDeploymentList corev1alpha1.ClusterObjectDeploymentList
+
+	err := h.client.List(ctx, &clusterObjectDeploymentList)
+	if err != nil {
+		return nil, fmt.Errorf("getting objectdeployments: %w", err)
+	}
+	for _, n := range clusterObjectDeploymentList.Items {
+		for _, owner := range n.OwnerReferences {
+			if ownerUid == owner.UID {
+				return &n, nil
+			}
+		}
+	}
+	return nil, nil
+}
+
+func (h *History) GetClusterObjectSetByOwner(ctx context.Context, ownerUid types.UID) (*[]corev1alpha1.ClusterObjectSet, error) {
+	var clusterObjectSetList corev1alpha1.ClusterObjectSetList
+
+	err := h.client.List(ctx, &clusterObjectSetList)
+	if err != nil {
+		return nil, fmt.Errorf("getting objectsets: %w", err)
+	}
+	var objectSets []corev1alpha1.ClusterObjectSet
+	for _, n := range clusterObjectSetList.Items {
+		for _, owner := range n.OwnerReferences {
+			if ownerUid == owner.UID {
+				objectSets = append(objectSets, n)
+			}
+		}
+	}
+	return &objectSets, nil
+}
+
 func (h *History) GetPackageByName(ctx context.Context, name string) (*corev1alpha1.Package, error) {
 	var packageList corev1alpha1.PackageList
 
-	err := h.client.List(ctx, &packageList, client.InNamespace("default"))
+	err := h.client.List(ctx, &packageList, client.InNamespace(h.Namespace))
 	if err != nil {
 		return nil, fmt.Errorf("getting packages: %w", err)
 	}
@@ -123,7 +216,7 @@ func (h *History) GetPackageByName(ctx context.Context, name string) (*corev1alp
 func (h *History) GetObjectDeploymentByName(ctx context.Context, name string) (*corev1alpha1.ObjectDeployment, error) {
 	var objectDeploymentList corev1alpha1.ObjectDeploymentList
 
-	err := h.client.List(ctx, &objectDeploymentList, client.InNamespace("default"))
+	err := h.client.List(ctx, &objectDeploymentList, client.InNamespace(h.Namespace))
 	if err != nil {
 		return nil, fmt.Errorf("getting objectdeployments: %w", err)
 	}
@@ -138,7 +231,7 @@ func (h *History) GetObjectDeploymentByName(ctx context.Context, name string) (*
 func (h *History) GetObjectDeploymentByOwner(ctx context.Context, ownerUid types.UID) (*corev1alpha1.ObjectDeployment, error) {
 	var objectDeploymentList corev1alpha1.ObjectDeploymentList
 
-	err := h.client.List(ctx, &objectDeploymentList, client.InNamespace("default"))
+	err := h.client.List(ctx, &objectDeploymentList, client.InNamespace(h.Namespace))
 	if err != nil {
 		return nil, fmt.Errorf("getting objectdeployments: %w", err)
 	}
@@ -155,7 +248,7 @@ func (h *History) GetObjectDeploymentByOwner(ctx context.Context, ownerUid types
 func (h *History) GetObjectSetByOwner(ctx context.Context, ownerUid types.UID) (*[]corev1alpha1.ObjectSet, error) {
 	var objectSetList corev1alpha1.ObjectSetList
 
-	err := h.client.List(ctx, &objectSetList, client.InNamespace("default"))
+	err := h.client.List(ctx, &objectSetList, client.InNamespace(h.Namespace))
 	if err != nil {
 		return nil, fmt.Errorf("getting objectsets: %w", err)
 	}
@@ -176,6 +269,8 @@ func (h *History) CobraCommand() *cobra.Command {
 		Short: historyShort,
 		Long:  historyLong,
 	}
+	f := cmd.Flags()
+	f.StringVarP(&h.Namespace, "namespace", "n", "", namespaceUse)
 
 	cmd.RunE = func(cmd *cobra.Command, args []string) (err error) {
 		if err := h.Complete(args); err != nil {
