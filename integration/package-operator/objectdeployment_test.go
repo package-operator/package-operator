9d28140e840373b0feba091e01b3e25def5f5f6d
Nico Schieder <nschieder@redhat.com>
2023-06-08 18:15:32 +0200

    Fix hash-collision fixture
    Signed-off-by: Nico Schieder <nschieder@redhat.com>

--- integration/package-operator/objectdeployment_test.go
+++ integration/package-operator/objectdeployment_test.go
@@ -108,7 +108,7 @@ func TestObjectDeployment_availability_and_hash_collision(t *testing.T) {
 	// Pre-Creating a ObjectSet that should conflict with Generation 2.
 	existingConflictObjectSet := &corev1alpha1.ObjectSet{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-objectdeployment-69b7c79bbf",
+			Name:      "test-objectdeployment-995cbf7d6",
 			Namespace: "default",
 		},
 		Spec: corev1alpha1.ObjectSetSpec{
