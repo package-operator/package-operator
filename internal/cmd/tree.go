e8b5ec092ca7dc40bbe270fee3cdba1fd7d36405
Alessandro Costa <alecosta.me@gmail.com>
2023-09-22 14:55:24 +0200

    Update tree subcommand with component flag

--- internal/cmd/tree.go
+++ internal/cmd/tree.go
@@ -72,8 +72,7 @@ func (t *Tree) RenderPackage(ctx context.Context, srcPath string, opts ...Render
 		return "", fmt.Errorf("loading package contents from folder: %w", err)
 	}
 
-	// TODO: handle components
-	pkg, err := packagecontent.PackageFromFiles(ctx, t.scheme, files, "")
+	pkg, err := packagecontent.PackageFromFiles(ctx, t.scheme, files, cfg.Component)
 	if err != nil {
 		return "", fmt.Errorf("parsing package contents: %w", err)
 	}
@@ -236,6 +235,7 @@ type RenderPackageConfig struct {
 	ClusterScope   bool
 	ConfigPath     string
 	ConfigTestcase string
+	Component      string
 }
 
 func (c *RenderPackageConfig) Option(opts ...RenderPackageOption) {
