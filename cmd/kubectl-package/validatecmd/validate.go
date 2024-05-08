7f3f079f52d30b25b5ee311875c66174b065cc9d
Ankit Kurmi <ankitkurmi152@gmail.com>
2024-05-08 17:30:15 +0530

    [PKO-41] Print affirmative message when package validation succeeds (#1137)
    * feat: [pko-41] Print affirmative message when package validation succeeds
    
    Signed-off-by: Ankit152 <ankitkurmi152@gmail.com>
    
    * chore: added expected output in integration tested
    
    Signed-off-by: Ankit152 <ankitkurmi152@gmail.com>
    
    ---------
    
    Signed-off-by: Ankit152 <ankitkurmi152@gmail.com>

--- cmd/kubectl-package/validatecmd/validate.go
+++ cmd/kubectl-package/validatecmd/validate.go
@@ -20,6 +20,7 @@ func NewCmd(validator Validator) *cobra.Command {
 		validateShort = "validate a package."
 		validateLong  = "validate a package. Target may be a source directory, " +
 			"a package in a tar[.gz] or a fully qualified tag if --pull is set."
+		validationSuccessMessage = "Package validated successfully!"
 	)
 
 	cmd := &cobra.Command{
@@ -52,6 +53,8 @@ func NewCmd(validator Validator) *cobra.Command {
 		if err := validator.ValidatePackage(cmd.Context(), validateOptions...); err != nil {
 			return fmt.Errorf("validating package: %w", err)
 		}
+
+		fmt.Fprint(cmd.OutOrStdout(), validationSuccessMessage)
 
 		return nil
 	}
