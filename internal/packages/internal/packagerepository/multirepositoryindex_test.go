3e2ca47895d49ee4fa88a5b0304bc90958464b7f
Alessandro Costa <alecosta.me@gmail.com>
2024-07-19 12:21:11 +0200

    Improve coverage in multirepositoryindex_test.go

--- internal/packages/internal/packagerepository/multirepositoryindex_test.go
+++ internal/packages/internal/packagerepository/multirepositoryindex_test.go
@@ -3,7 +3,10 @@ package packagerepository
 import (
 	"context"
 	"os"
+	"strings"
 	"testing"
+
+	"github.com/google/go-containerregistry/pkg/v1/empty"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -11,7 +14,9 @@ import (
 	"package-operator.run/internal/apis/manifests"
 )
 
-const multiRepositoryIndexFileSeed = `---
+const (
+	repoName                     = "hans"
+	multiRepositoryIndexFileSeed = `---
 apiVersion: manifests.package-operator.run/v1alpha1
 kind: Repository
 metadata:
@@ -31,15 +36,30 @@ metadata:
   creationTimestamp: null
   name: pkg.12345
 `
+)
 
-func TestMultiRepositoryIndex(t *testing.T) {
+func TestLoad(t *testing.T) {
 	t.Parallel()
 
-	const (
-		pkgName      = "pkg"
-		repo1Name    = "hans"
-		hansRepoFile = "testdata/hans.repo.yaml"
-	)
+	ctx := context.Background()
+	mri := NewMultiRepositoryIndex()
+	require.NoError(t, mri.LoadRepository(ctx, strings.NewReader(multiRepositoryIndexFileSeed)))
+
+	testMRI(ctx, t, mri)
+}
+
+func TestLoadError(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	mri := NewMultiRepositoryIndex()
+	require.Error(t, mri.LoadRepository(ctx, strings.NewReader("foobar")))
+}
+
+func TestLoadFromFile(t *testing.T) {
+	t.Parallel()
+
+	const hansRepoFile = "testdata/hans.repo.yaml"
 	require.NoError(t,
 		os.WriteFile(hansRepoFile, []byte(multiRepositoryIndexFileSeed), os.ModePerm))
 	t.Cleanup(func() {
@@ -48,53 +68,18 @@ func TestMultiRepositoryIndex(t *testing.T) {
 		}
 	})
 
-	entry2 := Entry{
-		RepositoryEntry: &manifests.RepositoryEntry{
-			Data: manifests.RepositoryEntryData{
-				Name:     "pkg",
-				Image:    "quay.io/xxx",
-				Digest:   "678",
-				Versions: []string{"v1.3.0"},
-			},
-		},
-		RepositoryName: repo1Name,
-	}
-	assert.Equal(t, "pkg.hans", entry2.FQDN())
-
-	mri := NewMultiRepositoryIndex()
-
 	ctx := context.Background()
+	mri := NewMultiRepositoryIndex()
 	require.NoError(t,
 		mri.LoadRepositoryFromFile(ctx, hansRepoFile))
 
-	// Read
-	entry1, err := mri.GetVersion(repo1Name, "pkg", "v1.2.3")
-	require.NoError(t, err)
-	latest, err := mri.GetLatestEntry(repo1Name, "pkg")
-	require.NoError(t, err)
-	assert.Equal(t, entry1, latest)
-
-	// Add entry 2
-	require.NoError(t, mri.Add(ctx, entry2))
-
-	// Check data after adding entry 2
-	latest, err = mri.GetLatestEntry(repo1Name, "pkg")
-	require.NoError(t, err)
-	assert.Equal(t, entry2, latest)
-	assert.Len(t, mri.ListEntries(repo1Name, "pkg"), 2)
-	vs, err := mri.ListVersions(repo1Name, "pkg")
-	require.NoError(t, err)
-	assert.Len(t, vs, 3)
-	byDigest, err := mri.GetDigest(repo1Name, "pkg", "12345")
-	require.NoError(t, err)
-	assert.Equal(t, entry1, byDigest)
-
-	require.NoError(t, mri.Remove(ctx, entry2))
+	testMRI(ctx, t, mri)
 
 	repo, err := mri.GetRepository("hans")
 	require.NoError(t, err)
 
 	require.NoError(t, os.MkdirAll("testdata", os.ModePerm))
+
 	file, err := os.Create(hansRepoFile)
 	require.NoError(t, err)
 	defer func() {
@@ -104,4 +89,142 @@ func TestMultiRepositoryIndex(t *testing.T) {
 	}()
 
 	require.NoError(t, repo.Export(ctx, file))
+}
+
+func TestLoadFromFileError(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	mri := NewMultiRepositoryIndex()
+	require.Error(t, mri.LoadRepositoryFromFile(ctx, "foobar.xyz"))
+}
+
+func TestLoadFromOCI(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	idx, err := LoadRepository(ctx, strings.NewReader(multiRepositoryIndexFileSeed))
+	require.NoError(t, err)
+	ociImg, err := SaveRepositoryToOCI(ctx, idx)
+	require.NoError(t, err)
+
+	mri := NewMultiRepositoryIndex()
+	require.NoError(t, mri.LoadRepositoryFromOCI(ctx, ociImg))
+
+	testMRI(ctx, t, mri)
+}
+
+func TestLoadFromOCIError(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	mri := NewMultiRepositoryIndex()
+	require.Error(t, mri.LoadRepositoryFromOCI(ctx, empty.Image))
+}
+
+func testMRI(ctx context.Context, t *testing.T, mri *MultiRepositoryIndex) {
+	t.Helper()
+
+	newEntry := entryFor(repoName, "pkg", "quay.io/xxx", "678", "v1.3.0")
+	assert.Equal(t, "pkg."+repoName, newEntry.FQDN())
+
+	// Read
+	entry1, err := mri.GetVersion(repoName, "pkg", "v1.2.3")
+	require.NoError(t, err)
+	latest, err := mri.GetLatestEntry(repoName, "pkg")
+	require.NoError(t, err)
+	assert.Equal(t, entry1, latest)
+
+	_, err = mri.GetVersion(repoName, "foo", "v1.2.3")
+	require.Error(t, err)
+	_, err = mri.GetLatestEntry(repoName, "foo")
+	require.Error(t, err)
+
+	_, err = mri.GetVersion("bar", "foo", "v1.2.3")
+	require.Error(t, err)
+	_, err = mri.GetLatestEntry("bar", "foo")
+	require.Error(t, err)
+
+	// Add entry 2
+	require.NoError(t, mri.Add(ctx, newEntry))
+
+	// Check data after adding entry 2
+	latest, err = mri.GetLatestEntry(repoName, "pkg")
+	require.NoError(t, err)
+	assert.Equal(t, newEntry, latest)
+
+	assert.Len(t, mri.ListEntries(repoName, "pkg"), 2)
+	assert.Empty(t, mri.ListEntries(repoName, "foo"))
+	assert.Len(t, mri.ListAllEntries(), 2)
+
+	vs, err := mri.ListVersions(repoName, "pkg")
+	require.NoError(t, err)
+	assert.Len(t, vs, 3)
+	_, err = mri.ListVersions(repoName, "foo")
+	require.Error(t, err)
+
+	byDigest, err := mri.GetDigest(repoName, "pkg", "12345")
+	require.NoError(t, err)
+	assert.Equal(t, entry1, byDigest)
+	_, err = mri.GetDigest(repoName, "pkg", "67890")
+	require.Error(t, err)
+	_, err = mri.GetDigest(repoName, "foo", "67890")
+	require.Error(t, err)
+
+	require.NoError(t, mri.Remove(ctx, newEntry))
+
+	otherRepo := "other"
+	otherRepoEntry := entryFor("other", "pkg", "quay.io/yyy", "9AB", "v2.0.0")
+	assert.Equal(t, "pkg.other", otherRepoEntry.FQDN())
+
+	require.NoError(t, mri.Add(ctx, otherRepoEntry))
+
+	assert.Len(t, mri.ListEntries(repoName, "pkg"), 1)
+	_, err = mri.GetVersion(repoName, "pkg", "v1.2.3")
+	require.NoError(t, err)
+	_, err = mri.GetVersion(repoName, "pkg", "v2.0.0")
+	require.Error(t, err)
+	_, err = mri.GetDigest(repoName, "pkg", "12345")
+	require.NoError(t, err)
+	_, err = mri.GetDigest(repoName, "pkg", "9AB")
+	require.Error(t, err)
+
+	assert.Len(t, mri.ListEntries(otherRepo, "pkg"), 1)
+	_, err = mri.GetVersion(otherRepo, "pkg", "v1.2.3")
+	require.Error(t, err)
+	_, err = mri.GetVersion(otherRepo, "pkg", "v2.0.0")
+	require.NoError(t, err)
+	_, err = mri.GetDigest(otherRepo, "pkg", "12345")
+	require.Error(t, err)
+	_, err = mri.GetDigest(otherRepo, "pkg", "9AB")
+	require.NoError(t, err)
+
+	assert.Len(t, mri.ListAllEntries(), 2)
+
+	_, err = mri.GetRepository(otherRepo)
+	require.NoError(t, err)
+
+	require.NoError(t, mri.Remove(ctx, otherRepoEntry))
+
+	_, err = mri.GetRepository(otherRepo)
+	require.Error(t, err)
+
+	assert.Len(t, mri.ListEntries(repoName, "pkg"), 1)
+	assert.Empty(t, mri.ListEntries(otherRepo, "pkg"))
+	assert.Len(t, mri.ListAllEntries(), 1)
+}
+
+func entryFor(repoName, name, image, digest, version string) Entry {
+	entry := Entry{
+		RepositoryEntry: &manifests.RepositoryEntry{
+			Data: manifests.RepositoryEntryData{
+				Name:     name,
+				Image:    image,
+				Digest:   digest,
+				Versions: []string{version},
+			},
+		},
+		RepositoryName: repoName,
+	}
+	return entry
 }
