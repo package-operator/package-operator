bfa403deef8cedeb27d0432ada725347c9c64e33
Petr Babic <pbabic@redhat.com>
2024-08-19 16:14:44 +0200

    test: add CEL template function unit tests

--- internal/packages/internal/packagerender/template_test.go
+++ internal/packages/internal/packagerender/template_test.go
@@ -32,7 +32,8 @@ func TestRenderTemplates(t *testing.T) {
 			"test.yml.gotmpl":  template,
 		}
 		pkg := &packagetypes.Package{
-			Files: fm,
+			Files:    fm,
+			Manifest: &manifests.PackageManifest{},
 		}
 
 		ctx := context.Background()
@@ -61,7 +62,8 @@ func TestRenderTemplates(t *testing.T) {
 			"test.yaml.gotmpl": template,
 		}
 		pkg := &packagetypes.Package{
-			Files: fm,
+			Files:    fm,
+			Manifest: &manifests.PackageManifest{},
 		}
 
 		ctx := context.Background()
@@ -85,11 +87,91 @@ func TestRenderTemplates(t *testing.T) {
 			"test.yaml.gotmpl": template,
 		}
 		pkg := &packagetypes.Package{
-			Files: fm,
+			Files:    fm,
+			Manifest: &manifests.PackageManifest{},
 		}
 
 		ctx := context.Background()
 		err := RenderTemplates(ctx, pkg, tmplCtx)
 		require.Error(t, err)
 	})
+}
+
+func TestRenderTemplates_CelFunction(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		name       string
+		tmplCtx    packagetypes.PackageRenderContext
+		conditions []manifests.PackageManifestNamedCondition
+		template   string
+		result     string
+		err        error
+	}{
+		{
+			name:     "if else with cel",
+			template: `{{if cel "true && false"}}then{{else}}else{{end}}`,
+			result:   "else",
+		},
+		{
+			name: "context construction error",
+			conditions: []manifests.PackageManifestNamedCondition{
+				{
+					Name:       "invalid",
+					Expression: "invalid",
+				},
+			},
+			template: `{{cel "cond.invalid"}}`,
+			err:      errConstructingCelContext,
+		},
+		{
+			name: "reusable condition",
+			conditions: []manifests.PackageManifestNamedCondition{
+				{
+					Name:       "test_condition",
+					Expression: "true && false",
+				},
+			},
+			template: `{{cel "cond.test_condition"}}`,
+			result:   "false",
+		},
+		{
+			name: "template context access",
+			tmplCtx: packagetypes.PackageRenderContext{
+				Config: map[string]any{
+					"banana": "bread",
+				},
+			},
+			template: `{{cel ".config.banana == \"bread\""}}`,
+			result:   "true",
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			fm := packagetypes.Files{
+				"test.yaml.gotmpl": []byte(tc.template),
+			}
+			pkg := &packagetypes.Package{
+				Files: fm,
+				Manifest: &manifests.PackageManifest{
+					Spec: manifests.PackageManifestSpec{
+						Filters: manifests.PackageManifestFilter{
+							Conditions: tc.conditions,
+						},
+					},
+				},
+			}
+
+			ctx := context.Background()
+			err := RenderTemplates(ctx, pkg, tc.tmplCtx)
+
+			if tc.err == nil {
+				require.NoError(t, err)
+				assert.Equal(t, tc.result, string(fm["test.yaml"]))
+			} else {
+				assert.ErrorIs(t, err, tc.err)
+			}
+		})
+	}
 }
