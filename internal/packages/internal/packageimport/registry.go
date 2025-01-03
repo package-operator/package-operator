f04a47eb2cb7adefe4cf323e162e2cabb0e2ace0
Josh Gwosdz <jgwosdz@redhat.com>
2025-01-03 08:55:03 +0100

    PKO-183: add support to resolve own ServiceAccount and attached ImagePullSecrets to authenticate package image pull requests  (#1656)
    feat: add support to resolve own ServiceAccount and attached ImagePullSecrets to authenticate package image pull requests
    
    Signed-off-by: Josh Gwosdz <jgwosdz@redhat.com>
    Co-authored-by: Alex Sowitzki <dev@eqrx.net>

--- internal/packages/internal/packageimport/registry.go
+++ internal/packages/internal/packageimport/registry.go
@@ -2,11 +2,29 @@ package packageimport
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/google/go-containerregistry/pkg/crane"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	"package-operator.run/internal/packages/internal/packageimport/kubekeychain"
 	"package-operator.run/internal/packages/internal/packagetypes"
 )
+
+// Imports a RawPackage from a container image registry,
+// while supplying pull credentials which are dynamically discovered from the ServiceAccount PKO is running under.
+func FromRegistryInCluster(
+	ctx context.Context, uncachedClient client.Client, serviceAccount types.NamespacedName,
+	ref string, opts ...crane.Option,
+) (*packagetypes.RawPackage, error) {
+	chain, err := kubekeychain.FromServiceAccountPullSecrets(ctx, uncachedClient, serviceAccount)
+	if err != nil {
+		return nil, fmt.Errorf("creating keychain: %w", err)
+	}
+	opts = append(opts, crane.WithAuthFromKeychain(chain))
+	return FromRegistry(ctx, ref, opts...)
+}
 
 // Imports a RawPackage from a container image registry.
 func FromRegistry(
