1baec0692ed2ef865a1fa2fd3747494a9437a7c1
erdii <me@erdii.engineering>
2026-03-02 16:26:26 +0100

    chore(release): add comment about the expected data structure in RELEASE_ROTATION

--- misc/release_rotation.py
+++ misc/release_rotation.py
@@ -7,6 +7,8 @@ slack_webhook_url = os.getenv("SLACK_WEBHOOK_URL")
 if slack_webhook_url is None:
     raise RuntimeError("SLACK_WEBHOOK_URL environment variable not set")
 
+# Value of RELEASE_ROTATION must be a comma separated list of slack user ids.
+# For example: U123456,U98765,U237847
 rotation = os.getenv("RELEASE_ROTATION")
 if rotation is None:
     raise RuntimeError("RELEASE_ROTATION environment variable not set")
